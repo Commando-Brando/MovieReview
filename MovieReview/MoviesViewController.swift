8d670a06dd32c00cd2aab83584aacc692f78cc31
Brandon Muller <60548867+Commando-Brando@users.noreply.github.com>
2022-02-20 16:52:19 -0600

    First push for flimsier movie app

---
+++ MovieReview/MoviesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MoviesViewController.swift
+//  MovieReview
+//
+//  Created by Brandon Muller on 2/20/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Hello")
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
