d86c8f06f11ca79a297253d3d04e8a60597a80df
Brandon Muller <60548867+Commando-Brando@users.noreply.github.com>
2022-02-20 14:46:11 -0600

    Initial Commit

---
+++ MovieReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReview
+//
+//  Created by Brandon Muller on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
